5e1d106c493c2e24c616345ed9df09eb02bb8db0
mandeephanda <mandeep@stellartms.com>
2023-08-25 17:17:06 +0530

    Created Single Interface

--- src/components/NestedArray.tsx
+++ src/components/NestedArray.tsx
@@ -1,33 +1,43 @@
-import React from "react"
+import React from "react";
 
-interface nestedProps
-{
-  nestedObject:nameAge
+interface singleProps {
+  nestedObject: {
+    name: string;
+    age: number;
+    address: {
+      street: string;
+      city: string;
+      state: string;
+    };
+    hobbies:string[];
+  };
 }
 
-interface Address
-{
-    street:string,
-    city:string,
-    state:string
-}
+// interface nestedProps {
+//   nestedObject: nameAge;
+// }
 
-interface nameAge
-{
-     name:string,
-     age:number,
-     address:Address,
-     hobbies:string[],
-}
+// interface Address {
+//   street: string;
+//   city: string;
+//   state: string;
+// }
 
-const NestedArray = (props:nestedProps) => {
+// interface nameAge {
+//   name: string;
+//   age: number;
+//   address: Address;
+//   hobbies: string[];
+// }
+
+const NestedArray = (props: singleProps) => {
   return (
     <div>
       <h2>{props.nestedObject.address.state}</h2>
       <h2>{props.nestedObject.address.city}</h2>
       <h2>{props.nestedObject.name}</h2>
     </div>
-  )
+  );
 };
 
-export default NestedArray
+export default NestedArray;
